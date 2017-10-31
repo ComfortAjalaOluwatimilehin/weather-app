62785a61a0eb96e22deb6fcd7f36a2b7943dfb85
Comfort AJala <comfort.ajala@ajalacomfort.com>
2017-10-31 20:38:19 +0100

    added google api key

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -66,7 +66,7 @@ class Canvas extends Component{
                         <div id="canvas">
                             <Map
                                 isMarkerShown
-                                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDoaSE6zHK2WKiOKd3a0TiFAY2xRbHjelo"
                                 loadingElement={<div style={{ height: `100%` }} />}
                                 containerElement={<div style={{ height: `100%` }} />}
                                 mapElement={<div style={{ height: `100%` }} />}
